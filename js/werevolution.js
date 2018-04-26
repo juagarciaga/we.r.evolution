dca629754a105d2dcbfb1e94d73bf6e0c4994c32
Juliana Garcia <juagarciaga@bitbucket.org>
2018-04-26 18:09:22 -0300

    first angular

---
+++ js/werevolution.js
@@ -0,0 +1,116 @@
+angular.module('revoApp', []).controller('revoAppController', function ($scope) {
+    $scope.a = 1;
+    $scope.b = 2;
+
+    $scope.friends = [{
+            name: 'Alex Miasaki',
+            img: 'alex'
+        },
+        {
+            name: 'Aline Macedo',
+            img: 'gaba'
+        },
+        {
+            name: 'Aline Nakagawa de Oliveira',
+            img: 'aline'
+        },
+        {
+            name: 'Camilla Gomes',
+            img: 'cami'
+        },
+        {
+            name: 'Cris Gonçalves',
+            img: 'cris'
+        },
+        {
+            name: 'Douglas Teixeira',
+            img: 'doug'
+        },
+        {
+            name: 'Edvaldo Junior',
+            img: 'junior'
+        },
+        {
+            name: 'Gabriela Bravo',
+            img: 'gabi'
+        },
+        {
+            name: 'Guilherme Chehade',
+            img: 'avats'
+        },
+        {
+            name: 'Gustavo Bravo',
+            img: 'gus'
+        },
+        {
+            name: 'Gustavo Lopes Costa',
+            img: 'gus'
+        }, {
+            name: 'Herisson Redi',
+            img: 'redi'
+        }, {
+            name: 'Jefferson Silva',
+            img: 'jef'
+        }, {
+            name: 'Juliana Garcia',
+            img: 'ju'
+        }, {
+            name: 'Leansi Souza',
+            img: 'le'
+        }, {
+            name: 'Leonardo Gonçalves',
+            img: 'ja'
+        }, {
+            name: 'Leonardo Haddad',
+            img: 'leo'
+        }, {
+            name: 'Lucas Mascia',
+            img: 'lucas'
+        }, {
+            name: 'Luiz A. B. Alvarenga',
+            img: 'poodle'
+        }, {
+            name: 'Manu Saunier',
+            img: 'manu'
+        }, {
+            name: 'Marina Alves',
+            img: 'mari'
+        }, {
+            name: 'Mauricio Holzmann',
+            img: 'mau'
+        }, {
+            name: 'Milene',
+            img: 'mi'
+        }, {
+            name: 'Natasha Ribeiro',
+            img: 'nat'
+        }, {
+            name: 'Nilau Gonçalves',
+            img: 'ni'
+        }, {
+            name: 'Paolo Chiappin',
+            img: 'paolo'
+        }, {
+            name: 'Pavlo Leonidovich',
+            img: 'pavlo'
+        }, {
+            name: 'Priscilla de Siqueira',
+            img: 'pri'
+        }, {
+            name: 'Robson Bruno',
+            img: 'rob'
+        }, {
+            name: 'Samara Gonçalves',
+            img: 'sami'
+        }, {
+            name: 'Sebastian Ruiz',
+            img: 'seba'
+        }, {
+            name: 'Talita Ferraz',
+            img: 'tali'
+        }, {
+            name: 'Vitor Morinishi',
+            img: 'vitor'
+        }
+    ];
+});
